4152e7115719d675029174afd300637cf2745243
Anton Ovesen <anton@ovesen-mail.dk>
2024-01-27 20:43:41 +0100

    feat: add rotate component

---
+++ TrollsVsElves/TrollsVsElves/Scripts/Components/RotateComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Xna.Framework.Input;
+using TrollsVsElves.Core.Abstractions;
+using TrollsVsElves.Scripts.Services;
+
+namespace TrollsVsElves.Scripts.Components;
+
+public class RotateComponent : Component, IUpdateableComponent, ITransient
+{
+    private readonly InputHandlerService _inputHandler;
+
+    public RotateComponent(InputHandlerService inputHandler)
+    {
+        _inputHandler = inputHandler;
+    }
+
+    public float RotateSpeed { get; set; }
+
+    public void OnUpdate(float deltaTime)
+    {
+        if (_inputHandler.KeyboardState.IsKeyDown(Keys.A)) Transform.TranslateRotation(1 * deltaTime);
+        if (_inputHandler.KeyboardState.IsKeyDown(Keys.D)) Transform.TranslateRotation(-1 * deltaTime);
+    }
+}
--- TrollsVsElves/TrollsVsElves/Scripts/GameObjects/Player.cs
+++ TrollsVsElves/TrollsVsElves/Scripts/GameObjects/Player.cs
@@ -1,29 +1,27 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
-using TrollsVsElves.Core.Abstractions;
 using TrollsVsElves.Core.Components;
 using TrollsVsElves.Core.Services;
 using TrollsVsElves.Scripts.Components;
 
 namespace TrollsVsElves.Scripts.GameObjects;
 
-public class Player : GameObject, IUpdateableComponent
+public class Player : GameObject
 {
-    private readonly SpriteRenderer _spriteRenderer;
     private readonly MoveWithCursorComponent _moveWithCursorComponent;
+    private readonly RotateComponent _rotateComponent;
+    private readonly SpriteRenderer _spriteRenderer;
 
     public Player(IServiceProvider services)
     {
-        _spriteRenderer = AddComponent(services.GetRequiredService<SpriteRenderer>());
         _moveWithCursorComponent = AddComponent(services.GetRequiredService<MoveWithCursorComponent>());
+        _rotateComponent = AddComponent(services.GetRequiredService<RotateComponent>());
+        _spriteRenderer = AddComponent(services.GetRequiredService<SpriteRenderer>());
+
+        _rotateComponent.RotateSpeed = 1;
 
         var textureFactory = services.GetRequiredService<TextureFactory>();
 
         _spriteRenderer.Sprite.Texture = textureFactory.CreateIfNotExists("Sprites/Square");
-    }
-
-    public void OnUpdate()
-    {
-        
     }
 }
